a69a7db00a943febd3d1e2a0f664e80d18951b9b
Youssef <yboudout84@gmail.com>
2024-10-18 09:58:39 +0200

    Implementation PokemonTrainerFactory.java

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -0,0 +1,23 @@
+package fr.univavignon.pokedex.api;
+
+public class PokemonTrainerFactory implements IPokemonTrainerFactory {
+
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+
+        if (team == null) {
+            throw new IllegalArgumentException("Team cannot be null");
+        }
+
+        if (pokedexFactory == null) {
+            throw new IllegalArgumentException("PokedexFactory cannot be null");
+        }
+
+        IPokedex pokedex = pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory());
+
+        return new PokemonTrainer(name, team, pokedex);
+    }
+}
