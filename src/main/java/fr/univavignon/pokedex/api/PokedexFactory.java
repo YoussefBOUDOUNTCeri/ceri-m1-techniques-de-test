17aa2758a05ec13260f2aacd2318999442099679
Youssef <yboudout84@gmail.com>
2024-11-15 10:34:02 +0100

    PokedexFactory

--- src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
@@ -1,11 +1,16 @@
 package fr.univavignon.pokedex.api;
 
-
 public class PokedexFactory implements IPokedexFactory {
 
     @Override
     public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
-        // Return a new Pokedex instance with the provided metadataProvider and pokemonFactory
+        if (metadataProvider == null) {
+            throw new NullPointerException("metadataProvider cannot be null");
+        }
+        if (pokemonFactory == null) {
+            throw new NullPointerException("pokemonFactory cannot be null");
+        }
+
         return new Pokedex(metadataProvider, pokemonFactory);
     }
 }
