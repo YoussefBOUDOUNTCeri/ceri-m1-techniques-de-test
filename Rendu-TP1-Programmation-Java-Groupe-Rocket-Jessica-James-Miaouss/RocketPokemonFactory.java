240b03f1aff156968a466ec71202d2e4198d6cc6
Youssef <yboudout84@gmail.com>
2024-12-10 11:53:44 +0100

    test cheklist

--- Rendu-TP1-Programmation-Java-Groupe-Rocket-Jessica-James-Miaouss/RocketPokemonFactory.java
+++ Rendu-TP1-Programmation-Java-Groupe-Rocket-Jessica-James-Miaouss/RocketPokemonFactory.java
@@ -13,18 +13,22 @@ public class RocketPokemonFactory implements IPokemonFactory {
 	
 	private static Map<Integer, String> index2name;
 	static {
+		// Probleme : Variable aMap non déclarée comme final
 		Map<Integer, String> aMap = new HashMap<Integer, String>();
         aMap.put(-1, "Ash's Pikachu");
         aMap.put(0, "MISSINGNO");
         aMap.put(1, "Bulbasaur");
         //TODO : Gotta map them all !
+
         index2name = UnmodifiableMap.unmodifiableMap(aMap);
 	}
 	
 	private static int generateRandomStat() {
 		int total = 0;
+		// Probleme : Méthode inefficace pour générer une statistique aléatoire
 		for(int i=0; i < 1000000; i++)
 		{
+			// Probleme : Création d'une nouvelle instance de Random à chaque itération
 			Random rn = new Random();
 		    int r = rn.nextInt(2);
 		    total = total + r;
@@ -34,15 +38,20 @@ public class RocketPokemonFactory implements IPokemonFactory {
 
 	@Override
 	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+		// Probleme : Absence de déclaration final pour les variables
 		String name;
 		if(!index2name.containsKey(index)) {
 			name = index2name.get(0);
 		} else {
 			name = index2name.get(index);
 		}
+		// Probleme : Absence de déclaration final pour les variables
 		int attack;
+		// Probleme : Absence de déclaration final pour les variables
 		int defense;
+		// Probleme : Absence de déclaration final pour les variables
 		int stamina;
+		// Probleme : Absence de déclaration final pour les variables
 		double iv;
 		if(index < 0) {
 			attack = 1000;
