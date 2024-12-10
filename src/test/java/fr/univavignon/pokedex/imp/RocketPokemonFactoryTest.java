0d66536c2d96d73e4c18175d1c5f0db16ddd9ea2
Youssef <yboudout84@gmail.com>
2024-12-10 10:57:54 +0100

    devbut TP6

---
+++ src/test/java/fr/univavignon/pokedex/imp/RocketPokemonFactoryTest.java
@@ -0,0 +1,45 @@
+package fr.univavignon.pokedex.imp;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.univavignon.pokedex.api.Pokemon;
+
+public class RocketPokemonFactoryTest {
+
+    private RocketPokemonFactory rocketPokemonFactory;
+
+    @Before
+    public void setUp() {
+        rocketPokemonFactory = new RocketPokemonFactory();
+    }
+
+    @Test
+    public void testCreatePokemonWithValidIndex() {
+        Pokemon pokemon = rocketPokemonFactory.createPokemon(1, 613, 64, 4000, 4);
+        assertNotNull(pokemon);
+        assertEquals(1, pokemon.getIndex());
+        assertEquals("Bulbasaur", pokemon.getName());
+    }
+
+    @Test
+    public void testCreatePokemonWithInvalidIndex() {
+        Pokemon pokemon = rocketPokemonFactory.createPokemon(-1, 613, 64, 4000, 4);
+        assertNotNull(pokemon);
+        assertEquals(-1, pokemon.getIndex());
+        assertEquals("Ash's Pikachu", pokemon.getName());
+        assertEquals(1000, pokemon.getAttack());
+        assertEquals(1000, pokemon.getDefense());
+        assertEquals(1000, pokemon.getStamina());
+    }
+
+    @Test
+    public void testCreatePokemonWithRandomStats() {
+        Pokemon pokemon = rocketPokemonFactory.createPokemon(0, 500, 55, 2000, 2);
+        assertNotNull(pokemon);
+        assertTrue(pokemon.getAttack() >= 0 && pokemon.getAttack() <= 15);
+        assertTrue(pokemon.getDefense() >= 0 && pokemon.getDefense() <= 15);
+        assertTrue(pokemon.getStamina() >= 0 && pokemon.getStamina() <= 15);
+    }
+}
